23b504f2562ac355a86c5e981c4301377caf3ee1
Clint Armstrong <clint@clintarmstrong.net>
2018-03-14 15:59:37 -0400

    windows fix

--- internal/pinger/control_windows.go
+++ internal/pinger/control_windows.go
@@ -3,6 +3,8 @@
 package pinger
 
 import (
+	"net"
+
 	"golang.org/x/net/icmp"
 	"golang.org/x/net/ipv4"
 	"golang.org/x/net/ipv6"
@@ -14,13 +16,13 @@ func setPacketCon(c *icmp.PacketConn) error {
 
 func listenPacket(c *icmp.PacketConn, r *recvMsg) error {
 	var err error
+	var src net.IPAddr
+	r.payloadLen, src, err = c.ReadFrom(r.payload)
 	switch {
 	case c.IPv4PacketConn() != nil:
-		r.v4cm = &ipv4.ControlMessage{}
-		r.payloadLen, r.v4cm.Src, err = c.ReadFrom(r.payload)
+		r.v4cm = &ipv4.ControlMessage{Src: src.IP}
 	case c.IPv6PacketConn() != nil:
-		r.v6cm = &ipv6.ControlMessage{}
-		r.payloadLen, r.v6cm.Src, err = c.ReadFrom(r.payload)
+		r.v6cm = &ipv6.ControlMessage{Src: src.IP}
 	}
 	return err
 }
