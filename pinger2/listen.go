89de61892fb3f079af10aa56b0395fff6b8c728b
Clint Armstrong <clint@clintarmstrong.net>
2018-03-17 12:08:49 -0400

    back to locks

--- pinger2/listen.go
+++ pinger2/listen.go
@@ -4,102 +4,136 @@ import (
 	"context"
 	"encoding/binary"
 	"net"
+	"sync"
 
 	"github.com/TrilliumIT/go-multiping/ping"
 )
 
-type index struct {
-	ip net.IP
-	id int
+// listen map index
+type lmI [18]byte
+
+// listen map entry
+type lmE struct {
+	cb  func(*ping.Ping)
+	ctx context.Context
 }
 
-func (i *index) key() [18]byte {
-	var r [18]byte
-	copy(r[0:16], i.ip.To16())
-	binary.LittleEndian.PutUint16(r[16:], uint16(i.id))
+func toLmI(ip net.IP, id int) lmI {
+	var r lmI
+	copy(r[0:16], ip.To16())
+	binary.LittleEndian.PutUint16(r[16:], uint16(id))
 	return r
 }
 
-type setter struct {
-	*index
-	cb func(*ping.Ping)
-}
-
-type getter struct {
-	*index
-	cb chan<- func(*ping.Ping)
-}
-
-type manager struct {
-	ctx   context.Context
-	setCh chan *setter
-	getCh chan *getter
-	delCh chan *index
-}
-
-func (m *manager) set(ip net.IP, id int, cb func(*ping.Ping)) {
-	set(m.ctx, m.setCh, ip, id, cb)
-}
-
-func set(ctx context.Context, set chan<- *setter, ip net.IP, id int, cb func(*ping.Ping)) {
-	select {
-	case set <- &setter{&index{ip, id}, cb}:
-	case <-ctx.Done():
+func newListenMap(ctx context.Context) *listenMap {
+	return &listenMap{
+		ctx: ctx,
+		m:   make(map[lmI]*lmE),
+		v4l: &listener{proto: 4},
+		v6l: &listener{proto: 6},
 	}
 }
 
-func (m *manager) get(ip net.IP, id int) func(*ping.Ping) {
-	return get(m.ctx, m.getCh, ip, id)
+type listenMap struct {
+	m   map[lmI]*lmE
+	l   sync.RWMutex
+	ctx context.Context
+	v4l *listener
+	v6l *listener
 }
 
-func get(ctx context.Context, get chan<- *getter, ip net.IP, id int) func(*ping.Ping) {
-	c := make(chan func(*ping.Ping))
+type listener struct {
+	proto   int
+	l       sync.RWMutex
+	running bool
+	wg      sync.WaitGroup
+	ctx     context.Context
+}
 
-	select {
-	case get <- &getter{&index{ip, id}, c}:
-	case <-ctx.Done():
+func (l *listener) run() {}
+
+func (l *listenMap) getL(ip net.IP) *listener {
+	if ip.To4() != nil {
+		return l.v4l
+	}
+	return l.v6l
+}
+
+type alreadyExistsError struct{}
+
+func (a *alreadyExistsError) Error() string {
+	return "already exists"
+}
+
+func (lm *listenMap) add(ip net.IP, id int, s *lmE) error {
+	idx := toLmI(ip, id)
+	err := lm.addIdx(idx, s)
+	if err != nil {
+		return err
+	}
+	l := lm.getL(ip)
+	l.wg.Add(1)
+	go func() {
+		<-s.ctx.Done()
+		lm.delIdx(idx)
+		l.wg.Done()
+	}()
+
+	l.l.RLock()
+	r := l.running
+	l.l.RUnlock()
+	if r {
 		return nil
 	}
 
-	select {
-	case cb := <-c:
-		return cb
-	case <-ctx.Done():
+	l.l.Lock()
+	if l.running {
+		l.l.Unlock()
+		return nil
 	}
+
+	l.running = true
+	var cancel func()
+	l.ctx, cancel = context.WithCancel(lm.ctx)
+	l.run()
+	l.l.Unlock()
+
+	go func() {
+		l.wg.Wait()
+		cancel()
+	}()
 	return nil
 }
 
-func (m *manager) del(ip net.IP, id int) {
-	del(m.ctx, m.delCh, ip, id)
-}
-
-func del(ctx context.Context, del chan<- *index, ip net.IP, id int) {
-	select {
-	case del <- &index{ip, id}:
-	case <-ctx.Done():
+func (l *listenMap) addIdx(idx lmI, s *lmE) error {
+	l.l.Lock()
+	_, ok := l.m[idx]
+	if ok {
+		l.l.Unlock()
+		return &alreadyExistsError{}
 	}
+	l.m[idx] = s
+	l.l.Unlock()
+	return nil
 }
 
-func (m *manager) run() {
-	run(m.ctx, m.setCh, m.getCh, m.delCh)
+func (l *listenMap) get(ip net.IP, id int) (*lmE, bool) {
+	return l.getIdx(toLmI(ip, id))
 }
 
-func run(ctx context.Context, set <-chan *setter, get <-chan *getter, del <-chan *index) {
-	callbacks := make(map[[18]byte]func(*ping.Ping))
-	for {
-		select {
-		case s := <-set:
-			callbacks[s.key()] = s.cb
-		case d := <-del:
-			delete(callbacks, d.key())
-		case g := <-get:
-			select {
-			case g.cb <- callbacks[g.key()]:
-			case <-ctx.Done():
-				return
-			}
-		case <-ctx.Done():
-			return
-		}
-	}
+func (l *listenMap) getIdx(idx lmI) (*lmE, bool) {
+	l.l.RLock()
+	s, ok := l.m[idx]
+	l.l.RUnlock()
+	return s, ok
+}
+
+func (l *listenMap) del(ip net.IP, id int) {
+	l.delIdx(toLmI(ip, id))
+}
+
+func (l *listenMap) delIdx(idx lmI) {
+	l.l.Lock()
+	delete(l.m, idx)
+	l.l.Unlock()
 }
