b5e7bac6f368233ad85b71ac6b0c5d470e97b255
Clint Armstrong <clint@clintarmstrong.net>
2018-03-17 16:09:22 -0400

    add callback to dst

--- pinger2/dst.go
+++ pinger2/dst.go
@@ -2,11 +2,14 @@ package pinger
 
 import (
 	"context"
+
+	"github.com/TrilliumIT/go-multiping/ping"
 )
 
 type Dst struct {
 	ctx    context.Context
 	cancel func()
+	cb     func(*ping.Ping, error)
 }
 
 func (d *Dst) Cancel() {
--- pinger2/newDst.go
+++ pinger2/newDst.go
@@ -3,6 +3,8 @@ package pinger
 import (
 	"context"
 	"time"
+
+	"github.com/TrilliumIT/go-multiping/ping"
 )
 
 func newDst(ctx context.Context, cancel func()) *Dst {
@@ -12,30 +14,35 @@ func newDst(ctx context.Context, cancel func()) *Dst {
 	}
 }
 
-func (p *Pinger) NewDst() *Dst {
-	return newDst(context.WithCancel(p.ctx))
+func (d *Dst) setCallback(cb func(*ping.Ping, error)) *Dst {
+	d.cb = cb
+	return d
 }
 
-func (p *Pinger) NewDstWithTimeout(timeout time.Duration) *Dst {
-	return newDst(context.WithTimeout(p.ctx, timeout))
+func (p *Pinger) NewDst(cb func(*ping.Ping, error)) *Dst {
+	return newDst(context.WithCancel(p.ctx)).setCallback(cb)
 }
 
-func (p *Pinger) NewDstWithDeadline(d time.Time) *Dst {
-	return newDst(context.WithDeadline(p.ctx, d))
+func (p *Pinger) NewDstWithTimeout(cb func(*ping.Ping, error), timeout time.Duration) *Dst {
+	return newDst(context.WithTimeout(p.ctx, timeout)).setCallback(cb)
 }
 
-func NewDst() *Dst {
-	return Default().NewDst()
+func (p *Pinger) NewDstWithDeadline(cb func(*ping.Ping, error), d time.Time) *Dst {
+	return newDst(context.WithDeadline(p.ctx, d)).setCallback(cb)
 }
 
-func NewDstWithContext(ctx context.Context) *Dst {
-	return newDst(context.WithCancel(ctx))
+func NewDst(cb func(*ping.Ping, error)) *Dst {
+	return Default().NewDst(cb)
 }
 
-func NewDstWithTimeout(timeout time.Duration) *Dst {
-	return Default().NewDstWithTimeout(timeout)
+func NewDstWithContext(ctx context.Context, cb func(*ping.Ping, error)) *Dst {
+	return newDst(context.WithCancel(ctx)).setCallback(cb)
 }
 
-func NewDstWithDeadline(d time.Time) *Dst {
-	return Default().NewDstWithDeadline(d)
+func NewDstWithTimeout(cb func(*ping.Ping, error), timeout time.Duration) *Dst {
+	return Default().NewDstWithTimeout(cb, timeout)
+}
+
+func NewDstWithDeadline(cb func(*ping.Ping, error), d time.Time) *Dst {
+	return Default().NewDstWithDeadline(cb, d)
 }
