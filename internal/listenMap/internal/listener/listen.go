abb6c61ee0d5c108ec23e6fe8a02deea23c4bdcc
Clint Armstrong <clint@clintarmstrong.net>
2018-03-18 16:33:49 -0400

    move process into listen

--- internal/listenMap/internal/listener/listen.go
+++ internal/listenMap/internal/listener/listen.go
@@ -9,7 +9,6 @@ import (
 	"golang.org/x/net/icmp"
 
 	"github.com/TrilliumIT/go-multiping/internal/listenMap/internal/messages"
-	"github.com/TrilliumIT/go-multiping/internal/listenMap/internal/process"
 	"github.com/TrilliumIT/go-multiping/ping"
 )
 
@@ -141,8 +140,22 @@ func (l *Listener) Run(getCb func(net.IP, uint16) func(context.Context, *ping.Pi
 				continue
 			}
 			r.Recieved = time.Now()
-			go process.ProcessMessage(ctx, r, getCb)
+			go processMessage(ctx, r, getCb)
 		}
 	}()
 	return nil
+}
+
+func processMessage(ctx context.Context, r *messages.RecvMsg, getCb func(net.IP, uint16) func(context.Context, *ping.Ping)) {
+	p := r.ToPing()
+	if p == nil {
+		return
+	}
+
+	cb := getCb(p.Dst, uint16(p.ID))
+	if cb == nil {
+		return
+	}
+
+	cb(ctx, p)
 }
