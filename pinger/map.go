f698b85ea6063b69f47ddcbdd8efe65b2f547ec5
Clint Armstrong <clint@clintarmstrong.net>
2018-03-18 19:48:10 -0400

    separate map files

--- pinger/map.go
+++ pinger/map.go
@@ -7,27 +7,6 @@ import (
 	"github.com/TrilliumIT/go-multiping/ping"
 )
 
-type pendingPkt struct {
-	cancel func()
-	p      *ping.Ping
-	l      sync.Mutex
-	err    error
-}
-
-func (p *pendingPkt) wait(ctx context.Context, pm *pendingMap, cb func(*ping.Ping, error), done func()) {
-	<-ctx.Done()
-	p.l.Lock()
-	pm.del(uint16(p.p.Seq))
-	if ctx.Err() == context.DeadlineExceeded && p.err == nil {
-		p.err = ErrTimedOut
-	}
-
-	cb(p.p, p.err)
-	p.l.Unlock()
-
-	done()
-}
-
 type pendingMap struct {
 	m map[uint16]*pendingPkt
 	l sync.Mutex
