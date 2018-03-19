dba9c9c52ff36d692031a4fcf8df392d24305607
Clint Armstrong <clint@clintarmstrong.net>
2018-03-19 13:07:48 -0400

    document workers, default to -1

--- pinger/conn.go
+++ pinger/conn.go
@@ -14,16 +14,18 @@ type Conn struct {
 
 // NewConn returns a new Conn
 func NewConn() *Conn {
-	return &Conn{
+	c := &Conn{
 		lm: listenMap.NewListenMap(),
 	}
+	c.lm.SetWorkers(-1)
+	return c
 }
 
-// SetWorkers sets a number of workers to process incoming packets
-// By default each incoming packet fires off a new goroutine to process it and call the callback
-// Setting workers changes this behavior so that n goroutines are already running and waiting to process packets.
-// This may provide better performance than dynamic goroutines. But if the workers are filled this can lead to dropped ping responses.
-// This change will not take effect until all running pings on Conn are stopped
+// SetWorkers sets a number of workers to process incoming packets and distribute them to the appropriate handlers
+// -1 enables automatic worker allocation. Any time packet processing would block, a new worker is started. Workers remain active until all active pings stop.
+// 0 disalbes workers, causing each incoming packet to start a new goroutine to handle it. This can cause incoming packets to be missed.
+// A postive number pre-allocates a set number of workers
+// <-1 disables workers entirely and synchronously distributes the incoming packet before listening for a new one.
 func (c *Conn) SetWorkers(n int) {
 	c.lm.SetWorkers(n)
 }
