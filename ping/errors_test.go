2c1b9b5401335c9b0b34c8c3d41a52e640829b70
Clint Armstrong <clint@clintarmstrong.net>
2018-03-23 05:38:46 -0400

    add test plan

--- ping/errors_test.go
+++ ping/errors_test.go
@@ -17,3 +17,16 @@ func TestDupListener(t *testing.T) {
 	assert.Equal(listenmap.ErrAlreadyExists, DefaultConn().lm.Add(context.Background(), net.ParseIP("127.0.0.1"), 5, nil))
 	cancel()
 }
+
+/*
+
+test filling ids
+for i := 0; i < 1<<16
+addconn(localhost)
+add one more, check or error
+
+test filling seq
+for i to 1<<16
+send to timeout host with to = 0
+check that sending one more blocks
+*/
